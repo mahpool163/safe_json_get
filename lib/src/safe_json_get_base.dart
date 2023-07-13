3e4733224cd50f0f3fd329e15bc23566b2f60441
ma <ma_hpool@163.com>
2023-07-13 16:35:38 +0800

    init commit.

--- lib/src/safe_json_get_base.dart
+++ lib/src/safe_json_get_base.dart
@@ -1,8 +1,10 @@
+import 'dart:core';
+
 typedef JsonObject = Map<String, dynamic>;
 typedef JsonArray = List<dynamic>;
 
 extension JsonObjectX on JsonObject {
-  int? getInt(String key, [int? defaultValue]) {
+  int? valInt(String key, {int? defaultValue}) {
     return switch (this[key]) {
       int value => value,
       num value => value.toInt(),
@@ -10,7 +12,7 @@ extension JsonObjectX on JsonObject {
     };
   }
 
-  double? getDouble(String key, [double? defaultValue]) {
+  double? valDouble(String key, {double? defaultValue}) {
     return switch (this[key]) {
       double value => value,
       num value => value.toDouble(),
@@ -18,25 +20,25 @@ extension JsonObjectX on JsonObject {
     };
   }
 
-  String? getString(String key, [String? defaultValue]) => switch (this[key]) {
+  String? valString(String key, {String? defaultValue}) => switch (this[key]) {
         String value => value,
         dynamic value => value?.toString() ?? defaultValue,
       };
 
-  bool? getBool(String key, [bool? defaultValue]) => switch (this[key]) {
+  bool? valBool(String key, {bool? defaultValue}) => switch (this[key]) {
         bool value => value,
         dynamic value =>
           bool.tryParse('$value', caseSensitive: false) ?? defaultValue,
       };
 
-  List<R>? getList<R>(String key, [List<R>? defaultValue]) =>
+  List<R>? valList<R>(String key, {List<R>? defaultValue}) =>
       switch (this[key]) {
         List<R> value => value,
         List value => value.whereType<R>().toList().cast<R>(),
         _ => defaultValue,
       };
 
-  JsonObject? getJson(String key, [JsonObject? defaultValue]) =>
+  JsonObject? valJson(String key, {JsonObject? defaultValue}) =>
       switch (this[key]) {
         JsonObject value => value,
         Map value => value.map((key, value) => MapEntry('$key', value)),
@@ -52,4 +54,22 @@ extension JsonObjectX on JsonObject {
         JsonArray _ => true,
         _ => false,
       };
+
+  int getInt(String key, {int defaultValue = 0}) =>
+      valInt(key, defaultValue: defaultValue)!;
+
+  double getDouble(String key, {double defaultValue = .0}) =>
+      valDouble(key, defaultValue: defaultValue)!;
+
+  String getString(String key, {String defaultValue = ''}) =>
+      valString(key, defaultValue: defaultValue)!;
+
+  bool getBool(String key, {bool defaultValue = false}) =>
+      valBool(key, defaultValue: defaultValue)!;
+
+  List<R> getList<R>(String key, {List<R> defaultValue = const []}) =>
+      valList(key, defaultValue: defaultValue)!;
+
+  JsonObject getJson(String key, {JsonObject defaultValue = const {}}) =>
+      valJson(key, defaultValue: defaultValue)!;
 }
